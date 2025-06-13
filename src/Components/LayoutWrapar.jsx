e37c3d9a5e30199d718663e58ccfcaec24a6c49c
Hey-het <hetalbpatelsw@gmail.com>
2025-06-13 14:22:05 +0100

    try to solve the error

--- src/Components/LayoutWrapar.jsx
+++ src/Components/LayoutWrapar.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useState } from 'react';
-import Header from '@/components/Header';
+import Header from "@/Components/Header"
 import CreateSignIn from '@/Components/CreateSignIn';
 
 export default function ClientLayoutWrapper({ children }) {
